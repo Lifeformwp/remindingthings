3c9614e8c4a059e4b13541612048edf5eeb52113
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-06-27 23:09:42 +0300

    remindingthings-0.2-added-task-saving-feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,68 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder() {
+        this.props.addReminder(this.state.text);
+    }
+
+    renderReminders() {
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-8">
+                {
+                reminders.map(reminder => {
+                    return (
+                        <li key={reminder.id} className="list-group-item">
+                            <div>{reminder.text}</div>
+                        </li>
+                    )
+                })
+                }
+                </ul>
+                );
+
+    }
+
     render() {
         return (
             <div className="App">
                 <div className="title">
                     Reminding Things
                 </div>
-                <div className="form-inline">
+                <div className="form-inline reminder-form">
                     <div className="form-group">
                         <input
                             className="form-control"
                             placeholder="I have to..."
+                            onChange={event => this.setState({text: event.target.value})}
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={() => this.addReminder()}
                     >
                         Add reminder
                     </button>
+                    { this.renderReminders() }
                 </div>
             </div>
         )
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        reminders: state
+    }
+}
+export default connect(mapStateToProps, { addReminder })(App);
